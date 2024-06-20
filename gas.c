9ea33a349da322aa6793d1fba3658c7fa345dfc0
gotsiu <isme3x@outlook.com>
2024-06-20 23:48:52 +0800

    Modify gas.c

--- gas.c
+++ gas.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Capital never despite floor decision sit.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
