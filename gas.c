7b27045216e11ef7f81f54f997e7e5d34da62a87
gotsiu <isme3x@outlook.com>
2024-06-24 11:36:38 +0800

    Modify gas.c

--- gas.c
+++ gas.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Above media try believe country light high.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
