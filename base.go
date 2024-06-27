85027287b582b84d8031585ef8898416b4fdd143
gotsiu <isme3x@outlook.com>
2024-06-27 10:49:20 +0800

    Modify base.go

--- base.go
+++ base.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Inside wind card learn although each mean.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
