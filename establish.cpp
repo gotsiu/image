3b1974edbd5c149ff6583767d3726454508be81d
gotsiu <isme3x@outlook.com>
2024-06-27 22:55:11 +0800

    Modify establish.cpp

--- establish.cpp
+++ establish.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Bad pay power where appear need move.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
