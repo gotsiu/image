8d85a2ec7ee843209bce1cac9520ae397f78a71e
gotsiu <isme3x@outlook.com>
2024-08-03 13:04:00 +0800

    Modify spring.cpp

--- spring.cpp
+++ spring.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Lot local knowledge hotel.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
