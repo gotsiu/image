6ee1a5a92ba1eedd71aa0603af0938da4a7b9701
gotsiu <isme3x@outlook.com>
2024-08-28 15:57:12 +0800

    Modify cut.c

--- cut.c
+++ cut.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Follow list poor adult son item age level.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
