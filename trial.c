f1e0d21c692cc51ecf0c4933c2c9f0efd0a302fa
gotsiu <isme3x@outlook.com>
2024-09-11 13:35:42 +0800

    Modify trial.c

--- trial.c
+++ trial.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Month network exactly field left marriage mind add.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
