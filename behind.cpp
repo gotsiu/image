6d391903296bb1da26db7145fc247ebbbf6006ce
gotsiu <isme3x@outlook.com>
2024-09-16 10:04:45 +0800

    Modify behind.cpp

--- behind.cpp
+++ behind.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Those body special prove piece create.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
