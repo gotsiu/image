425ef0b0c5b552427776195fe77b037215c0dbcc
gotsiu <isme3x@outlook.com>
2024-09-20 19:43:58 +0800

    Modify trial.c

--- trial.c
+++ trial.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Score detail suddenly government red religious then.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
