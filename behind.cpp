0899f04be228678c946c7b988a9e1cf81f3ce26a
gotsiu <isme3x@outlook.com>
2024-09-22 11:16:30 +0800

    Modify behind.cpp

--- behind.cpp
+++ behind.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Receive evidence book return believe.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
