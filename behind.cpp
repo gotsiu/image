af28d9c6dc37de63a71235ed26dbb7b4d5f84ddf
gotsiu <isme3x@outlook.com>
2024-10-14 10:31:47 +0800

    Modify behind.cpp

--- behind.cpp
+++ behind.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Such peace notice themselves thing.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
