ceef77f09a2a663ee7fa7f5b1f7c1058dc4b3153
gotsiu <isme3x@outlook.com>
2024-10-31 13:22:06 +0800

    Modify reason.go

--- reason.go
+++ reason.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Voice chair body soon our amount help.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
