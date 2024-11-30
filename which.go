abcf6fb186678864980463d5638bb4c5586fe999
gotsiu <isme3x@outlook.com>
2024-11-30 23:53:45 +0800

    Modify which.go

--- which.go
+++ which.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Soldier company pattern executive address.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
