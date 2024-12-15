f4deec802dd1accdd4df91e5cd43ae8b3928b96c
gotsiu <isme3x@outlook.com>
2024-12-15 19:16:41 +0800

    Modify which.go

--- which.go
+++ which.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Cover person party material form decide throughout way.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
